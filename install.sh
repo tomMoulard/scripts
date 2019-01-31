b8d46b81d812582d1488a008d26a4c556f3f1ab6
Tom Moulard <tom.moulard@epita.fr>
2019-01-31 17:53:13 +0100

    install: reformating

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ ! -d "$HOME/.scripts" ]; then
-	mkdir -p "$HOME/.scripts"
+    mkdir -p "$HOME/.scripts"
 fi
 
 NAME="$(basename "$0")"
 
 for FILE in *.sh; do
-	if [ "$NAME" != "$FILE" ]; then
-		ln -s "$PWD/$FILE" "$HOME/.scripts"
-	fi
+    if [ "$NAME" != "$FILE" ]; then
+        ln -s "$PWD/$FILE" "$HOME/.scripts"
+    fi
 done
