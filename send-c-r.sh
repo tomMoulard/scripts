17d374c6cfb0136574ac2a103935df8690c1ca5c
Tom Moulard <tom.moulard@epita.fr>
2019-01-31 17:53:25 +0100

    send-c-r: reformating

--- send-c-r.sh
+++ send-c-r.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xdotool windowactivate "$(xdotool search --class "$1" | tail -1)" \
-	key ctrl+r windowactivate "$(xdotool getactivewindow)"
+    key ctrl+r windowactivate "$(xdotool getactivewindow)"
